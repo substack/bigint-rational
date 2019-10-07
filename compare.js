d1cc033d977d816dbbd6bcabcba63eb040e2d564
substack <substack@bits.coop>
2019-10-06 19:53:42 -1000

    simplify compare()

--- compare.js
+++ compare.js
@@ -1,8 +1,6 @@
-var bn = require('./lib/bn.js')
-
 module.exports = function compare (a, b) {
-  var x = a[0] * bn.abs(b[1]) * bn.sign(a[1])
-  var y = b[0] * bn.abs(a[1]) * bn.sign(b[1])
+  var x = a[0] * b[1]
+  var y = b[0] * a[1]
   if (x === y) return 0
   return x < y ? -1 : +1
 }
